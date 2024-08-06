e01696a027288880e8ae6b6f5c957ac7603e09e3
richard <whoami@hotmail.com>
2024-08-06 10:28:03 +0800

    debug

--- cpp/tom_vs_jerry.c
+++ cpp/tom_vs_jerry.c
@@ -8,6 +8,8 @@
  */
 #define _MV_OFFSET  	10
 
+#define _COLLIDE_PIX  	50
+
 /**
  * 窗口宽度 单位：像素
  */
@@ -18,7 +20,12 @@
  */
 #define _WINDOW_HEIGHT 	600
 
-#define _MV_INTERVAL_MS 20*1000
+#define _MV_INTERVAL_US 5*1000
+
+#define JERRY_X_INIT 	0
+#define JERRY_Y_INIT 	300
+#define TOM_X_INIT 		800
+#define TOM_Y_INIT 		300
 
 /**
  * 顶层容器，放置两个可固定位置的图标
@@ -35,16 +42,35 @@ static GtkWidget *jerry;
  */
 static GtkWidget *tom;
 
+/**
+ * 显示当前状态信息文本
+ */
 static GtkWidget *status_bar;
+
+/**
+ * jerry 得分
+ */
+static GtkWidget *jerry_score;
+
+/**
+ * tom 得分
+ */
+static GtkWidget *tom_score;
+
+static GtkWidget *timer;
+
+
 static gint status_bar_id;
 
 static int jerry_key;
 
 static int tom_key;
 
-static int jerry_collide_to_wall = 0;
+static int jerry_collide_to_wall;
 
-static int tom_collide_to_wall = 0;
+static int tom_collide_to_wall;
+
+static int is_game_over;
 
 struct Action {
 	int role;
@@ -94,6 +120,31 @@ void on_button_clicked(GtkButton *button, gpointer data) {
     refresh_status(buff);
 }
 
+void reset_game() {
+	jerry_key 				= 0;
+	tom_key 				= 0;
+	is_game_over 			= 0;
+	jerry_collide_to_wall 	= 0;
+	tom_collide_to_wall 	= 0;
+	gtk_fixed_move (GTK_FIXED (fixed), jerry, 	JERRY_X_INIT, 	JERRY_Y_INIT);
+	gtk_fixed_move (GTK_FIXED (fixed), tom, 	TOM_X_INIT, 	TOM_Y_INIT);
+	gchar *buff = g_strdup_printf ("game restart");
+	refresh_status(buff);
+}
+
+int check_meet_each_other() {
+	gint j_x = jerry->allocation.x;
+	gint j_y = jerry->allocation.y;
+	gint t_x = tom->allocation.x;
+	gint t_y = tom->allocation.y;
+	if(-_COLLIDE_PIX < j_x - t_x && j_x - t_x < _COLLIDE_PIX
+		&& -_COLLIDE_PIX < j_y - t_y && j_y - t_y < _COLLIDE_PIX ) {
+		is_game_over = 1;
+		return 1;
+	}
+	return 0;
+}
+
 
 int mv_widget(int role, GtkWidget *widget, int x_offset, int y_offset) {
 	gint x = widget->allocation.x;
@@ -120,6 +171,9 @@ int mv_widget(int role, GtkWidget *widget, int x_offset, int y_offset) {
 		gchar *buff = g_strdup_printf ("%s collide to north/south wall (%d, %d)",
 			role == 0 ? "jerry": "tom", x, y);
 		refresh_status(buff);
+	} else if (check_meet_each_other()){
+		gchar *buff = g_strdup_printf ("tom has caught jerry, game over");
+		refresh_status(buff);
 	} else {
 		gtk_fixed_move (GTK_FIXED (fixed), widget, x + x_offset, y + y_offset);
 		if(tom_collide_to_wall || jerry_collide_to_wall) {
@@ -128,11 +182,10 @@ int mv_widget(int role, GtkWidget *widget, int x_offset, int y_offset) {
 			} else {
 				jerry_collide_to_wall = 0;
 			}
-			gchar *buff = g_strdup_printf ("");
+			gchar *buff = g_strdup_printf (" ");
 			refresh_status(buff);
 		}
 	}
-
 	return 0;
 }
 
@@ -234,7 +287,7 @@ void* mv_tom(void* tdt) {
 	gchar *buff= g_strdup_printf ("tom action started");
 	refresh_status(buff);
 	while(1) {
-		if(tom_key) {
+		if (tom_key && !is_game_over) {
 			mv_role_by_key_press(1, tom_key);
 		}else {
 //			g_print("do_nothing_for_tom_action\n");
@@ -242,7 +295,7 @@ void* mv_tom(void* tdt) {
 //			gtk_statusbar_push (GTK_STATUSBAR (status_bar), status_bar_id, buff);
 //			g_free (buff);
 		}
-		g_usleep(_MV_INTERVAL_MS);
+		g_usleep(_MV_INTERVAL_US);
 	}
 	return NULL;
 }
@@ -252,7 +305,7 @@ void* mv_jerry(void* tdt) {
 	gchar *buff= g_strdup_printf ("jerry action started");
 	refresh_status(buff);
 	while(1) {
-		if(jerry_key) {
+		if (jerry_key && !is_game_over) {
 			mv_role_by_key_press(0, jerry_key);
 		} else {
 //			g_print("do_nothing_for_jerry_action\n");
@@ -260,7 +313,7 @@ void* mv_jerry(void* tdt) {
 //			gtk_statusbar_push (GTK_STATUSBAR (status_bar), status_bar_id, buff);
 //			g_free (buff);
 		}
-		g_usleep(_MV_INTERVAL_MS);
+		g_usleep(_MV_INTERVAL_US);
 	}
 	return NULL;
 }
@@ -271,7 +324,6 @@ void* mv_jerry(void* tdt) {
 gboolean on_key_pressed(GtkWidget *widget,
 		GdkEventKey *event, gpointer user_data) {
 //	g_print("_on_key_pressed %c\n", event->keyval);
-	gchar *buff;
 	switch(event->keyval) {
 	    case 'w':
 	    case 'W':
@@ -283,8 +335,6 @@ gboolean on_key_pressed(GtkWidget *widget,
 	    case 'S':
 	    	if(jerry_key !=event->keyval){
 				jerry_key = event->keyval;
-//				g_print("set_jerry_key_on_key_pressed %c\n", jerry_key);
-				buff = g_strdup_printf ("set_jerry_key_on_key_pressed %c", jerry_key);
 	    	}
 	        break;
 	    case 'i':
@@ -301,16 +351,17 @@ gboolean on_key_pressed(GtkWidget *widget,
 		case 65364:
 			if(tom_key !=event->keyval){
 				tom_key = event->keyval;
-//				g_print("set_tom_key_on_key_pressed %c\n", tom_key);
-				buff = g_strdup_printf ("set_tom_key_on_key_pressed %c", tom_key);
+			}
+			break;
+		case 32:
+			if(is_game_over) {
+				reset_game();
 			}
 			break;
 	    default:
-//	    	g_print("nothing_done_for_key_pressed %c\n", event->keyval);
-	    	buff = g_strdup_printf ("nothing_done_for_key_pressed %c", event->keyval);
 	    	break;
 	}
-//	refresh_status(buff);
+
 	return FALSE;
 }
 
@@ -357,8 +408,11 @@ gboolean on_key_released(GtkWidget *widget,
 }
 
 int main(int argc, char *argv[]) {
-	jerry_key = 0;
-	tom_key = 0;
+	jerry_key 				= 0;
+	tom_key 				= 0;
+	is_game_over 			= 0;
+	jerry_collide_to_wall 	= 0;
+	tom_collide_to_wall 	= 0;
 
 //	pthread_t t1;
 //	pthread_create(&t1, NULL, &mv_jerry, NULL);
@@ -392,8 +446,8 @@ int main(int argc, char *argv[]) {
     GtkWidget *box2 = pic_label_box ("tom.png", "Tom");
     gtk_container_add (GTK_CONTAINER (tom), box2);
 
-    gtk_fixed_put(GTK_FIXED(fixed), jerry, 0, 300);
-    gtk_fixed_put(GTK_FIXED(fixed), tom, 800, 300);
+    gtk_fixed_put(GTK_FIXED(fixed), jerry, JERRY_X_INIT, JERRY_Y_INIT);
+    gtk_fixed_put(GTK_FIXED(fixed), tom, TOM_X_INIT, TOM_Y_INIT);
 //    gtk_container_add(GTK_CONTAINER(vbox), fixed);
 	gtk_box_pack_start(GTK_BOX (vbox), fixed, TRUE, TRUE, 0);
     // test move
